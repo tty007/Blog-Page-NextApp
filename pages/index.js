fb02bba073acf49a1b62d8d12af6bbb860a336e0
tty007 <tty@MacbookAir.local>
2022-09-26 15:33:37 +0900

    2column layout

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,7 @@ import Hero from '/components/hero'
 export default function Home() {
   return (
     <Container>
-      <Hero
-        title="CUBE"
-        subtitle="アウトプットしていくサイト"
-        imageOn
-      />
+      <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
     </Container>
   )
 }
