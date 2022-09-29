1647af6b664b9d223edebd48db1e8c330b3a1c5f
tty007 <tty@MacbookAir.local>
2022-09-29 21:24:37 +0900

    Image implement

--- pages/about.js
+++ pages/about.js
@@ -3,11 +3,26 @@ import Hero from 'components/hero'
 import PostBody from 'components/post-body'
 import Contact from 'components/contact'
 import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
+import Image from 'next/image'
+import eyecatch from 'images/about.jpeg'
 
 export default function About() {
 	return (
 		<Container>
 			<Hero title="About" subtitle="About development activities" />
+
+			<figure>
+				<Image
+					src={eyecatch}
+					alt=""
+					layout='responsive'
+					sizes='(min-width: 1152px) 1152px, 100vw'
+					priority
+					placeholder='blur'
+					// style={{transition: '2s'}}
+				/>
+			</figure>
+
 			<TwoColumn>
 				<TwoColumnMain>
 					<PostBody>
