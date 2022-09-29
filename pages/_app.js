1a613664c019f816437d3ad0dae6a7d08dbcbf3e
tty007 <tty@MacbookAir.local>
2022-09-29 22:30:56 +0900

    social icon implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import Layout from 'components/layout'
 import 'styles/globals.css'
 
+// Font Awesome
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
