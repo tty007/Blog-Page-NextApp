24cb3c29c59ad5bcc2393b6d3a88f2008d1cf5ac
tty007 <c7dc007@gmail.com>
2022-10-06 20:23:37 +0900

    implement html-react-parser

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,7 +1,10 @@
 import { client, getPostBySlug } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body' 
+import ConvertBody from 'components/convert-body'
 import Image from 'next/image'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
 
 export default function Schedule({
   title,
@@ -26,6 +29,15 @@ export default function Schedule({
             priority
           />
         </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar></TwoColumnSidebar>
+        </TwoColumn>
       </article>
     </Container>
   )
