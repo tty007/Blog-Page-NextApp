edfc217b568c8bdf27146d6c20b5445aa1c03962
tty007 <c7dc007@gmail.com>
2022-10-12 19:54:15 +0900

    make slug file

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,4 +1,6 @@
-import { client, getPostBySlug } from 'lib/api'
+import {getAllSlugs, getPostBySlug } from 'lib/api'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
 import PostBody from 'components/post-body' 
@@ -6,9 +8,12 @@ import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/image'
 import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
+import { eyecatchLocal } from 'lib/contents'
+import { getPlaiceholder } from 'plaiceholder'
 
-export default function Schedule({
+export default function Post({
   title,
+  description,
   publish,
   content,
   eyecatch,
@@ -16,6 +21,13 @@ export default function Schedule({
 }) {
   return (
     <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
       <article>
         <PostHeader title={title} subtitle="Blog Article" publish={publish} />
 
@@ -28,6 +40,8 @@ export default function Schedule({
             height={eyecatch.height}
             sizes="(min-width: 1152px) 1152px, 100vw"
             priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
 
@@ -46,11 +60,22 @@ export default function Schedule({
   )
 }
 
-export async function getStaticProps() {
-  const slug = 'schedule'
+export async function getStaticPaths() {
+  const allSlugs = await getAllSlugs()
+  return {
+    paths: allSlugs.map(({slug}) => `/blog/${slug}`),
+    fallback: false,
+  }
+}
 
+export async function getStaticProps(context) {
+  const slug = context.params.slug
   const post = await getPostBySlug(slug)
-  console.log(post)
+  const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
 
   return {
     // propsの中身はAppコンポーネントに渡され、ページコンポーネント上で利用できるようになる
@@ -58,8 +83,9 @@ export async function getStaticProps() {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
+      description: description
     }
   }
 
